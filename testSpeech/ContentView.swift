b5d30e1cd8082b60fa679dd96444f9ee3d2310e5
shiro <pres@sunyou.co.jp>
2024-12-09 23:57:40 +0900

    Initial Commit

---
+++ testSpeech/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testSpeech
+//
+//  Created by 衣斐司郎 on 2024/12/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testSpeech/testSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSpeechApp.swift
+//  testSpeech
+//
+//  Created by 衣斐司郎 on 2024/12/09.
+//
+
+import SwiftUI
+
+@main
+struct testSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
